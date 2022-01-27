c9fa7a855c52ddba3927c8a4f6ab9974ffe9e02e
Konstantin Lemko <lemko99@bk.ru>
2022-01-27 15:24:04 +0300

    feat: add redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { store } from './store';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 window.addEventListener('message', e => {
   if (e.data && typeof e.data === 'string' && e.data.match(/webpackHotUpdate/)) {
