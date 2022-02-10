d9a37c3b7fb69db73d106a323dd83a2f4876b119
Dmitry K <69163014+webdev163@users.noreply.github.com>
2022-02-10 22:48:11 +0400

    feat: disable game button on learnt page

--- src/components/Guide/SelectGame/SelectGame.tsx
+++ src/components/Guide/SelectGame/SelectGame.tsx
@@ -1,19 +1,36 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-import { useActions } from '../../../hooks/useActions';
+import { useTypedSelector } from '../../../hooks/useTypedSelector';
 import { Link } from 'react-router-dom';
 
 import styles from './SelectGame.module.scss';
 
 const SelectGame: FC = () => {
+  const { doneCounter } = useTypedSelector(state => state.guide);
+  const { isAuthorized } = useTypedSelector(state => state.auth);
+  const [isDisabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    if (isAuthorized && doneCounter > 19) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [doneCounter]);
+
   return (
     <PopupState variant="popover" popupId="demo-popup-menu">
       {popupState => (
         <React.Fragment>
-          <Button variant="contained" {...bindTrigger(popupState)} sx={{ fontSize: 20, marginLeft: 'auto' }}>
+          <Button
+            variant="contained"
+            {...bindTrigger(popupState)}
+            sx={{ fontSize: 20, marginLeft: 'auto' }}
+            disabled={isDisabled}
+          >
             Мини-игры
           </Button>
           <Menu {...bindMenu(popupState)}>
