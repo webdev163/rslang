8aa482d21438de5695704a45cc247906c4597b47
Konstantin Lemko <lemko99@bk.ru>
2022-02-12 01:17:38 +0300

    feat: clear location state after reload

--- src/hooks/useLocationFrom.ts
+++ src/hooks/useLocationFrom.ts
@@ -1,12 +1,18 @@
-import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { RouterParams, RouterState } from '../types/types';
 
 export const useLocationFrom = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   let from: RouterParams | undefined;
   if (location.state) {
     from = (location.state as RouterState).from;
   }
+
+  useEffect(() => {
+    if (from) navigate(location.pathname, {});
+  }, []);
 
   return from;
 };
