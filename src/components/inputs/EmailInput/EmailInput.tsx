60fb751af75dcc13ad73ce609925bd5782c09f80
komalapa <komalapa@gmail.com>
2022-02-12 21:59:33 +0400

    fix: validate preloaded email

--- src/components/inputs/EmailInput/EmailInput.tsx
+++ src/components/inputs/EmailInput/EmailInput.tsx
@@ -1,4 +1,4 @@
-import React, { FC, FormEvent, useCallback, useState } from 'react';
+import React, { FC, FormEvent, useCallback, useEffect, useState } from 'react';
 import { EmailInputProps } from './types';
 
 import styles from './EmailInput.module.scss';
@@ -6,13 +6,20 @@ import styles from './EmailInput.module.scss';
 const EmailInput: FC<EmailInputProps> = ({ label, onFulfilled, onInput, value }) => {
   const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
 
-  const handleInput = useCallback((e: FormEvent<HTMLInputElement>) => {
-    const input = e.currentTarget.value;
-
-    const validator = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(input);
-    if (validator) onInput(input);
+  const onValidEmail = useCallback((email: string): void => {
+    const validator = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    if (validator) onInput(email);
     onFulfilled(validator);
     setIsEmailValid(validator);
+  }, []);
+
+  const handleInput = useCallback((e: FormEvent<HTMLInputElement>) => {
+    const input = e.currentTarget.value;
+    onValidEmail(input);
+  }, []);
+
+  useEffect(() => {
+    if (value) onValidEmail(value);
   }, []);
 
   return (
