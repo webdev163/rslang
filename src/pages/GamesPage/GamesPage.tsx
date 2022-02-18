69ecb2bdd8646ba5fd9c1ef6ad3447a05ce7c777
Dmitry K <69163014+webdev163@users.noreply.github.com>
2022-02-18 15:35:58 +0400

    feat: improve ui on games page

--- src/pages/GamesPage/GamesPage.tsx
+++ src/pages/GamesPage/GamesPage.tsx
@@ -8,15 +8,33 @@ const GamesPage: FC = () => {
   return (
     <div>
       <div className={styles.wrapperInner}>
-        <h1 className={styles.title}>Games Page</h1>
-        <div className={styles.linksWrapper}>
+        <h1 className={styles.title}>Мини-игры</h1>
+        <div className={styles.gamesWrapper}>
+          <div className={styles.gamesItem}>
+            <Link className={styles.link} to="/games/audio">
+              <h2>Аудиовызов</h2>
+              <div className={styles.image}>
+                <img src="/assets/img/audiogame.jpg" alt="Игра Аудиовызов" />
+              </div>
+            </Link>
+          </div>
+          <div className={styles.gamesItem}>
+            <Link className={styles.link} to="/games/sprint">
+              <h2>Спринт</h2>
+              <div className={styles.image}>
+                <img src="/assets/img/sprintgame.jpg" alt="Игра Спринт" />
+              </div>
+            </Link>
+          </div>
+        </div>
+        {/* <div className={styles.linksWrapper}>
           <Link className={styles.link} to="/games/audio">
             Мини-игра &quot;Аудиовызов&quot;
           </Link>
           <Link className={styles.link} to="/games/sprint">
             Мини-игра &quot;Спринт&quot;
           </Link>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </div>
